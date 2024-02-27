efed589fe23b899037f8e2b9257e65e17896bb3f
Jorge Blanco <jorgeblancosanchez.jbs@hotmail.com>
2024-02-27 09:26:56 +0100

    Lab 4 de algoritmia

--- lab/lab4/ws/Ordenacion/src/p2/RapidoInsercionTiempos.java
+++ lab/lab4/ws/Ordenacion/src/p2/RapidoInsercionTiempos.java
@@ -6,29 +6,36 @@ para los 3 supuestos de orden incial (ordenado, inverso y aleatorio) */
 public class RapidoInsercionTiempos
 {
 
-static int []v;
 
 public static void main (String arg [])
 {
-long t1,t2;
-
-
 	
-	v=new int [16000000];
+	final boolean PRINT_NUMBERS = false;
+	long t1,t2;
+
+	int[] kValues = {5,10,20,30,50,100,200,500,1000};
+	
+	for(int j=0;j<kValues.length;j++) {
+		
+		System.out.println("Tiempos para k = "+kValues[j]);
+	
+		int[] v=new int [16000000];
 
   
-	Vector.ordenAleatorio(v);
+		Vector.ordenAleatorio(v);
 		
-  	t1 = System.currentTimeMillis();
+  		t1 = System.currentTimeMillis();
 	  
-	RapidoInsercion.rapidoInsercion(v,100,0,16000000-1);
+  		RapidoInsercion.rapidoInsercion(v,kValues[j],0,16000000-1);
 	
-	for(int i=0;i<100;i++) {
-		System.out.print(v[i]);
-	}
+		if(PRINT_NUMBERS) {
+		for(int i=0;i<100;i++) {
+			System.out.print(v[i*1000]+" ");
+		}}
          
-	t2 = System.currentTimeMillis();
+		t2 = System.currentTimeMillis();
 
-  	System.out.println (16000000+"\t"+(t2-t1));
+  		System.out.println (16000000+"\t"+(t2-t1));
+	}
 }
 }
