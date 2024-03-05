901c224524f27767eba6a83490f377266517d6df
Jorge Blanco <jorgeblancosanchez.jbs@hotmail.com>
2024-03-05 11:33:11 +0100

    Merge

--- lab/lab5/ws/OrdenacionPractica3/src/p3/Merge.java
+++ lab/lab5/ws/OrdenacionPractica3/src/p3/Merge.java
@@ -2,23 +2,70 @@ package p3;
 
 public class Merge {
 
-	public static void Mezcla(int[] vector,int[] volcado,int inf,int sup) {
-		if(sup-inf==0) {
-			volcado[inf]=vector[inf];
-			return;
-		}else {
-			int m = sup-inf/2;
-				Mezcla(vector,volcado,inf,m);
-				Mezcla(vector,volcado,m+1,sup);
+	/**
+	 * Algoritmo de Mezcla
+	 * @param vector a ordenar
+	 * @param inf valor inferior del vector, vector[inf] debe existir
+	 * @param sup valor superior del vector, vector[sup] debe existir
+	 */
+	public static void mezcla(int[] vector,int inf,int sup) {
+		if(sup-inf>0) {
+			int m = (sup+inf)/2;
+				mezcla(vector,inf,m);
+				mezcla(vector,m+1,sup);
 				
-				Combinar(volcado,inf,m);
-				Combinar(volcado,m,sup);
+				combinar(vector,inf,m,sup);
 		}
 	}
 	
-	public static final void Combinar(int[] vector,int inf, int sup) {
-		for(int i=inf;i<sup;i++) {
+	public static final void combinar(int[] vector,int inf, int sup,int superiorVector2) {
+			int[] vectorAux= new int[superiorVector2-inf+1];
 			
+			int contador=0;
+
+			int izquierdo = inf;
+			int medio = sup;
+			int derecho = sup+1;
+			
+			while(izquierdo<=medio && derecho<=superiorVector2) {
+				if(vector[izquierdo]>=vector[derecho]) {
+					vectorAux[contador]=vector[derecho];
+					derecho++;
+				}else {
+					vectorAux[contador]=vector[izquierdo];
+					izquierdo++;
+				}
+				contador++;
+			}
+			
+			while(izquierdo<= medio) {
+				vectorAux[contador]=vector[izquierdo];
+				contador++;
+				izquierdo++;
+			}
+			
+			while(derecho<=superiorVector2) {
+				vectorAux[contador]=vector[derecho];
+				contador++;
+				derecho++;
+			}
+			
+			for(int i=0;i<vectorAux.length;i++) {
+				vector[inf+i]=vectorAux[i];
+			}
 		}
+	
+	public static void main (String arg []) 
+	{
+		int[] v= {28,9,14,39,39,1,25};
+		
+		mezcla(v,0,v.length-1);
+		
+		for(int i=0;i<v.length;i++) {
+			System.out.print(v[i]+",");
+		}
+		
+		
+		
 	}
 }
