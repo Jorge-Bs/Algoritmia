a3e62c05928a48d12a25b58ec4a82409600b1e6b
Jorge Blanco Sánchez <UO293697@uniovi.es>
2024-03-19 19:42:13 +0100

    Archivos clase

---
+++ lab/lab7/p5/ws/Patron/src/p5/Reader.java
@@ -0,0 +1,33 @@
+package p5;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Reader {
+
+	public static List<String> textos(String archivo) throws FileNotFoundException,IOException {
+		List<String> textos = new ArrayList<>();
+		try {
+			BufferedReader rd  = new BufferedReader(new FileReader(archivo));
+			
+			
+			while(rd.ready()) {
+				String line = rd.readLine();
+				String parts[] = line.split(" ");
+				textos.add(parts[0]);
+			}
+			rd.close();
+			return textos;
+					
+			
+		} catch (FileNotFoundException e) {
+			throw new FileNotFoundException("No se ha encontrado el fichero");
+		}catch(IOException e) {
+			throw new IOException("Se ha producido un error con la lectura del fichero");
+		}
+	}
+}
