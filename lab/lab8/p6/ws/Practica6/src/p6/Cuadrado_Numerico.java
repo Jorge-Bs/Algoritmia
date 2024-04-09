acbea9c777983bdf46dc663b24a7055a83c070cc
Jorge Blanco <jorgeblancosanchez.jbs@hotmail.com>
2024-04-09 16:41:37 +0200

    Lab 8

--- lab/lab8/p6/ws/Practica6/src/p6/Cuadrado_Numerico.java
+++ lab/lab8/p6/ws/Practica6/src/p6/Cuadrado_Numerico.java
@@ -1,5 +1,6 @@
 package p6;
 
+import java.awt.desktop.SystemEventListener;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.PriorityQueue;
@@ -7,85 +8,107 @@ import java.util.PriorityQueue;
 public class Cuadrado_Numerico {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[][] values= leerArchivos("test01.txt");
+		//int[][] values = {{6,DIV,EMPTY,3},{SUM,NOT_VALID,SUB,NOT_VALID},{EMPTY,MUL,EMPTY,12},{10,NOT_VALID,-1,NOT_VALID}};
+		operar(values);
 	}
 	
 	static final int MAX_VALUE=9;
-	static final int DIV=-5;
-	static final int MUL=-2;
-	static final int SUM=-3;
-	static final int SUB=-4;
-	static final int EMPTY=-6;
+	static final int DIV=-500;
+	static final int MUL=-200;
+	static final int SUM=-300;
+	static final int SUB=-400;
+	static final int EMPTY=-600;
 	
-	static final int NOT_VALID=-1;
-	
-	boolean restul=false;
-	
-	int values[][] = {{6,-5,NOT_VALID,3},{-3,EMPTY,-4,EMPTY},{NOT_VALID,-22,NOT_VALID,12}};
+	static final int NOT_VALID=-100;
+
 	
 	public void Cuadrado_Numerico(String fileName) throws FileNotFoundException, IOException {
 	}
 
-	private void leerArchivos(String fileName) {
-		
-	}
-	
-	private void operar(int valores[][],int fila,int columna) {
-			for(int i=fila;i<valores.length-1;i+=2) {
-				for(int j=columna;j<valores[i].length-1;j+=2) {
-					for(int k=1;k<=MAX_VALUE;k++) {
-						if(valores[i][j]==NOT_VALID) {
-							valores[i][j]=k;
-						}
-						operar(valores,i,j);
-						if(comprobarResultado(valores)){
-							System.out.print("Solucion encontrada");
-						}
-					}
+	private static int[][] leerArchivos(String fileName) {
+        try {
+            return Reader.textos(fileName);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+//----------------------------------------------------------------------------------------------------------------------
+	private static void operar(int[][] valores){
+		int[] siguiente = buscarSiguiente(valores);
+		if(siguiente[0]!=-1){
+			for(int i =1;i<=MAX_VALUE;i++){
+				valores[siguiente[0]][siguiente[1]]=i;
+				if(comprobarResultado2(valores)){
+					printMatriz(valores);
+					System.out.print("Solucion encontrada");
 				}
-			}						
-	}
-	
-	private boolean comprobarResultado(int[][] valores) {
-		boolean result = true;
-		for(int i=0;i<valores.length-1;i+=2) {
-			int resultadoFila=valores[i][0];
-			int resultadoColumna=valores[0][i];
-			for(int j=2;j<valores.length-1;j+=2) {
-				if(valores[i][j]==NOT_VALID ) {
-					return true;
-				}
-				resultadoFila += calculo(resultadoFila,valores[i][j-1],valores[i][j]);
-				resultadoColumna+= calculo(resultadoColumna,valores[j-1][i],valores[j][i]);
+				operar(valores);
 			}
-			if(resultadoFila!=valores[i][valores.length-1]) {
-				result = false;
-			}else if(resultadoColumna!=valores[valores.length-1][i]) {
-				result = false;
-			}
+			valores[siguiente[0]][siguiente[1]]=EMPTY;
 		}
-		return result;
 	}
 
-	private int calculo(int resultadoFila, int operacion, int valor) {
-		try {
-			switch(operacion) {
-			case DIV:
-				return resultadoFila/valor;
-			case MUL:
-				return resultadoFila*valor;
-			case SUM:
-				return resultadoFila+valor;
-			default:
-				return resultadoFila-valor;
+	private static int[] buscarSiguiente(int[][] valores){
+		for(int i=0;i< valores.length-1;i+=2){
+			for(int j=0;j< valores.length-1;j+=2){
+				if(valores[i][j]==EMPTY){
+					return  new int[]{i,j};
+				}
 			}
+		}
+		return new int[]{-1};
+	}
+
+	private static boolean comprobarResultado2(int[][] valores){
+		for(int i=0;i<valores.length-1;i+=2){
+			if(!comprobarFila(valores,i) || !comprobarColumna(valores,i)){
+				return false;
+			}
+		}
+		return  true;
+	}
+
+	private static boolean comprobarColumna(int[][] valores,int columna){
+		int suma = valores[0][columna];
+		for(int i=2;i<valores.length-1;i+=2){
+			suma = calculo(suma,valores[i-1][columna],valores[i][columna]);
+		}
+		return valores[valores.length-1][columna]==suma;
+	}
+
+	private static boolean comprobarFila(int[][] valores, int fila){
+		int suma = valores[fila][0];
+		for(int i=2;i<valores.length-1;i+=2){
+			suma = calculo(suma,valores[fila][i-1],valores[fila][i]);
+		}
+		return valores[fila][valores.length-1]==suma;
+	}
+
+	private static int calculo(int resultadoFila, int operation, int valor) {
+		try {
+			return switch (operation) {
+				case DIV -> resultadoFila / valor;
+				case MUL -> resultadoFila * valor;
+				case SUM -> resultadoFila + valor;
+				case SUB -> resultadoFila - valor;
+				default -> throw new IllegalStateException("Se ha procesado una operación invalida");
+			};
 		}catch(ArithmeticException e) {
 			return resultadoFila;
 		}
 	}
-	
-	
 
+//----------------------------------------------------------------------------------------------------------------------
+	private static void printMatriz(int[][] valores) {
+        for (int[] valor : valores) {
+            for (int j = 0; j < valores.length; j++) {
+                System.out.print(valor[j] + " ");
+            }
+            System.out.print("\n");
+        }
+
+	}
 
 }
--- lab/lab8/p6/ws/Practica6/src/p6/Reader.java
+++ lab/lab8/p6/ws/Practica6/src/p6/Reader.java
@@ -4,30 +4,80 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Reader {
 
-	public static List<String> textos(String archivo) throws FileNotFoundException,IOException {
+	public static int[][]  textos(String archivo) throws FileNotFoundException,IOException {
 		
 		try {
 			BufferedReader rd  = new BufferedReader(new FileReader(archivo));
-			
+			int[][] valores;
+
+
+
+
+			var linea = rd.readLine();
+			String[] parts  = linea.split("\r");
+			int tam = Integer.parseInt(parts[0]);
+			valores = new int[2*tam][2*tam];
+
+
+			int valor;
+			int fila=0;
 			
 			while(rd.ready()) {
-				String line = rd.readLine();
-				String parts[] = line.split(" ");
-				textos.add(parts[0]);
+				linea = rd.readLine();
+				parts  = linea.split(" ");
+				if(parts[0].equals("=")){
+					continue;
+				}
+				for (int i = 0; i < valores.length-1; i++) {
+					if(i>= parts.length){
+						valores[fila][i] = Cuadrado_Numerico.NOT_VALID;
+					}else {
+						valor = toValue(parts[i]);
+						valores[fila][i] = valor;
+					}
+				}
+				valores[fila][valores.length-1] = toValue(parts[parts.length-1]);
+				fila++;
 			}
-			rd.close();
-			return textos;
-					
+			for(int i=1;i< valores.length;i+=2){
+				int[] fil = new int[valores.length];
+				fil[0] = valores[i][0];
+				for(int j=1;j< valores.length-1;j++){
+					if(j%2==0){
+						fil[j] = valores[i][j-1];
+					}else{
+						fil[j] = Cuadrado_Numerico.NOT_VALID;
+					}
+
+				}
+				fil[fil.length-2]=valores[i][valores.length-1];
+				fil[fil.length-1]=Cuadrado_Numerico.NOT_VALID;
+				valores[i]=fil;
+			}
+			return valores;
 			
 		} catch (FileNotFoundException e) {
 			throw new FileNotFoundException("No se ha encontrado el fichero");
 		}catch(IOException e) {
 			throw new IOException("Se ha producido un error con la lectura del fichero");
 		}
+	}
+
+	private static int toValue(String valor) {
+        return switch (valor) {
+            case "+" -> Cuadrado_Numerico.SUM;
+            case "-" -> Cuadrado_Numerico.SUB;
+            case "*" -> Cuadrado_Numerico.MUL;
+            case "/" -> Cuadrado_Numerico.DIV;
+            case "?" -> Cuadrado_Numerico.EMPTY;
+			case " " -> Cuadrado_Numerico.NOT_VALID;
+            default -> Integer.parseInt(valor);
+        };
 	}
 }
